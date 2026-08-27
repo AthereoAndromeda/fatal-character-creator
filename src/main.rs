2f3ac2ee5d101d50696b5a53762727c3984884d9
AthereoAndromeda <athereoandromeda@gmail.com>
2026-08-27 14:03:02 +0800

    refactor(main): Cleanup main

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,7 @@ mod dice;
 mod simulator;
 use simulator::*;
 
-use crate::{
-    chapter_1::{Character, gender::Gender, race::Race},
-    dice::Dice as _,
-};
+use crate::chapter_1::Character;
 
 mod chapter_1;
 mod chapter_3;
@@ -15,14 +12,6 @@ const TEST_SEED: u64 = 100;
 
 fn main() {
     let mut sim = GameSimulator::new();
-    let dice_roll = sim.roll_1d20();
-    println!("{dice_roll}");
-
-    let race = Race::roll_random(&mut sim);
-    dbg!(race);
-
-    let gender = Gender::roll_random(&mut sim, &race);
-    dbg!(gender);
 
     let character = Character::roll_character(&mut sim);
     dbg!(character);
