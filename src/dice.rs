b7fee5e229beee8f6e88771c01fa86b1f55c9834
AthereoAndromeda <athereoandromeda@gmail.com>
2026-08-27 09:36:47 +0800

    feat(dice): Create `Dice` trait

---
+++ src/dice.rs
@@ -0,0 +1,58 @@
+use rand::RngExt;
+
+use crate::simulator::Simulator;
+
+macro_rules! gen_roll {
+    ($( ($amount:expr, $sides:expr) ),+ $(,)?) => {
+        pastey::paste! {
+            /// Creates a roll function for given dice
+            pub trait Dice: Simulator {
+                $(
+                    fn [<roll_ $amount d $sides>](&mut self) -> u64 {
+                        self.rng().random_range((1 * $amount)..=($sides * $amount))
+                    }
+                )+
+            }
+
+            impl<T: Simulator> Dice for T {}
+
+            // Generate a test case for each roll
+            #[cfg(test)]
+            mod roll_test {
+                use insta::*;
+
+                use crate::dice::Dice;
+                use crate::simulator::Simulator;
+                use crate::test::sim;
+
+                $(
+                    #[rstest::rstest]
+                    fn [<test_ $amount d $sides>](mut sim: impl Simulator) {
+                        let roll = sim.[<roll_ $amount d $sides>]();
+                        assert_ron_snapshot!(roll);
+                    }
+
+                    #[rstest::rstest]
+                    // May or may not take forever
+                    fn [<ensure_all_sides_reachable_ $amount d $sides>](mut sim: impl Simulator) {
+                        let roll_range = (1 * $amount)..($sides * $amount);
+                        let mut set: std::collections::HashSet<u64> = std::collections::HashSet::from_iter(roll_range);
+
+                        loop {
+                            let roll = sim.[<roll_ $amount d $sides>]();
+                            set.remove(&roll);
+                            if set.is_empty() { break };
+                        }
+                    }
+                )+
+            }
+        }
+    }
+}
+
+gen_roll!(
+    // (N die, sides of die)
+    (1, 20),
+    (2, 20),
+    (1, 100),
+);
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+mod dice;
 mod simulator;
 use rand::RngExt as _;
 use simulator::*;
 
+use crate::dice::Dice as _;
 
 mod chapter_1;
 
@@ -12,6 +14,9 @@ fn main() {
     let mut sim = TestSimulator::new(100);
     let num = sim.rng().random::<u32>();
     println!("{num}");
+
+    let dice_roll = sim.roll_1d20();
+    println!("{dice_roll}")
 }
 
 #[cfg(test)]
