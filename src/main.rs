66a1a181102ba74e48e2df781c5f19b54733b564
AthereoAndromeda <athereoandromeda@gmail.com>
2026-08-27 13:26:27 +0800

    test: Add `set_snapshot_suffix` macro

--- src/main.rs
+++ src/main.rs
@@ -31,6 +31,15 @@ fn main() {
 mod test {
     use crate::{TEST_SEED, TestSimulator};
 
+    #[macro_export]
+    macro_rules! set_snapshot_suffix {
+    ($($expr:expr),*) => {
+        let mut settings = insta::Settings::clone_current();
+        settings.set_snapshot_suffix(format!($($expr,)*));
+        let _guard = settings.bind_to_scope();
+    }
+}
+
     #[rstest::fixture]
     pub fn sim() -> TestSimulator {
         TestSimulator::new(TEST_SEED)
