2456b8f29b0778d6371a69233755a733f92b9d12
AthereoAndromeda <athereoandromeda@gmail.com>
2026-08-28 12:44:09 +0800

    refactor(gender): Move `GenderModifiers` inside `Gender`

--- src/chapter_1/gender.rs
+++ src/chapter_1/gender.rs
@@ -45,60 +45,60 @@ pub struct GenderModifiers {
 }
 
 impl GenderModifiers {
-    pub fn new(gender: &Gender) -> Self {
-        match gender {
-            Gender::Male => Self {
-                physique: PhysiqueModifiers {
-                    physical_fitness: 5,
-                    strength: 30,
-                    bodily_attractiveness: -3,
-                },
-
-                charisma: CharismaModifiers { face: -3 },
-
-                intelligence: IntelligenceModifiers {
-                    language: -2,
-                    math: 3,
-                    spatial: 3,
-                },
-
-                wisdom: WisdomModifiers {
-                    drive: 2,
-                    intuition: -5,
-                    reflection: -4,
-                },
-
-                temperament: TemperanceModifiers {
-                    sanguine: -2,
-                    choleric: 2,
-                },
+    pub fn male() -> Self {
+        Self {
+            physique: PhysiqueModifiers {
+                physical_fitness: 5,
+                strength: 30,
+                bodily_attractiveness: -3,
             },
 
-            Gender::Female => Self {
-                physique: PhysiqueModifiers {
-                    physical_fitness: -5,
-                    strength: -30,
-                    bodily_attractiveness: 3,
-                },
+            charisma: CharismaModifiers { face: -3 },
 
-                charisma: CharismaModifiers { face: 3 },
+            intelligence: IntelligenceModifiers {
+                language: -2,
+                math: 3,
+                spatial: 3,
+            },
 
-                intelligence: IntelligenceModifiers {
-                    language: 2,
-                    math: -3,
-                    spatial: -3,
-                },
+            wisdom: WisdomModifiers {
+                drive: 2,
+                intuition: -5,
+                reflection: -4,
+            },
 
-                wisdom: WisdomModifiers {
-                    drive: -2,
-                    intuition: 5,
-                    reflection: 4,
-                },
+            temperament: TemperanceModifiers {
+                sanguine: -2,
+                choleric: 2,
+            },
+        }
+    }
 
-                temperament: TemperanceModifiers {
-                    sanguine: 2,
-                    choleric: -2,
-                },
+    pub fn female() -> Self {
+        Self {
+            physique: PhysiqueModifiers {
+                physical_fitness: -5,
+                strength: -30,
+                bodily_attractiveness: 3,
+            },
+
+            charisma: CharismaModifiers { face: 3 },
+
+            intelligence: IntelligenceModifiers {
+                language: 2,
+                math: -3,
+                spatial: -3,
+            },
+
+            wisdom: WisdomModifiers {
+                drive: -2,
+                intuition: 5,
+                reflection: 4,
+            },
+
+            temperament: TemperanceModifiers {
+                sanguine: 2,
+                choleric: -2,
             },
         }
     }
@@ -106,12 +106,20 @@ impl GenderModifiers {
 
 #[derive(Debug)]
 pub enum Gender {
-    Male,
-    Female,
+    Male(GenderModifiers),
+    Female(GenderModifiers),
 }
 
 impl Gender {
-    pub fn roll_random(sim: &mut impl Simulator, race: &Race) -> Gender {
+    pub fn male() -> Self {
+        Self::Male(GenderModifiers::male())
+    }
+
+    pub fn female() -> Self {
+        Self::Female(GenderModifiers::female())
+    }
+
+    pub fn roll_random(sim: &mut impl Simulator, race: &Race) -> Self {
         let num = sim.roll_1d100();
 
         let final_num = match race {
@@ -128,9 +136,9 @@ impl Gender {
         };
 
         if final_num > 52 {
-            Gender::Male
+            Gender::Male(GenderModifiers::male())
         } else {
-            Gender::Female
+            Gender::Female(GenderModifiers::female())
         }
     }
 }
--- src/chapter_1/mod.rs
+++ src/chapter_1/mod.rs
@@ -1,13 +1,12 @@
 pub mod gender;
 pub mod race;
 
-use crate::{chapter_1::gender::GenderModifiers, chapter_3::Abilities, simulator::Simulator};
+use crate::{chapter_3::Abilities, simulator::Simulator};
 
 #[derive(Debug)]
 pub struct Character {
     race: race::Race,
     gender: gender::Gender,
-    modifiers: gender::GenderModifiers,
     abilities: Abilities,
 }
 
@@ -15,13 +14,11 @@ impl Character {
     pub fn roll_character(sim: &mut impl Simulator) -> Self {
         let race = race::Race::roll_random(sim);
         let gender = gender::Gender::roll_random(sim, &race);
-        let modifiers = GenderModifiers::new(&gender);
         let abilities = Abilities::roll_random(sim);
 
         Self {
             race,
             gender,
-            modifiers,
             abilities,
         }
     }
