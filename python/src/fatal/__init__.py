426586249b701f22e3485166a381bafa2fdf492a
Athereo <athereoandromeda@gmail.com>
2026-08-31 19:46:55 +0800

    refactor(python/name): Refactor name extractor

--- python/src/fatal/__init__.py
+++ python/src/fatal/__init__.py
@@ -1,6 +1,6 @@
 import json
 import os
-from itertools import takewhile
+from pprint import pp
 
 import pdfplumber
 from pdfplumber.page import Page
@@ -19,13 +19,12 @@ def parse_page(page: Page, is_even: bool) -> list[str]:
     return col
 
 
-def collect_pairs(arr: list[str]):
+def collect_pairs(arr: list[str]) -> list[list[str]]:
     tmp = []
-
     pairs = []
+
     for name in arr:
         splits = name.split("-")
-        # pp(splits)
         if all(x.isnumeric() for x in splits):
             pairs.append(name)
         else:
@@ -37,11 +36,149 @@ def collect_pairs(arr: list[str]):
     return tmp
 
 
-def parse_list(break_str: str, parsed_names):
-    arr = list(takewhile(lambda x: x[0] != break_str, parsed_names))
-    cor_len = len(arr)
-    arr = list(filter(lambda x: len(x) > 1, arr))
-    return arr, parsed_names[cor_len:]
+# Fix list since FATAL is a garbage game with a broken PDF
+#
+# Fixes:
+# - Page 950: 120 overlaps
+# - Page 951: Hall range is 301:350
+# - Page 955: 754:760 Reduald has gap
+# - Page 955: 271 overlaps
+# - Page 956: 817,818 missing
+# - Page 962: 192 Overlap
+def apply_fixes(arr: list[list[str]]) -> list[list[str]]:
+    tmp_arr = arr.copy()
+
+    # Remove collision
+    inc_idx = tmp_arr.index(["120-123", "Brettingham"])
+    tmp_arr[inc_idx] = ["121-123", "Brettingham"]
+
+    # Delete Hall
+    tmp_arr.remove(["301-350", "Hall"])
+
+    # Add missing index for Elven male 753
+    inc_idx = tmp_arr.index(["754-760", "Reduald"])
+    tmp_arr[inc_idx] = ["753-760", "Reduald"]
+
+    # Add missing index for Elven male 817-818
+    inc_idx = tmp_arr.index(["819-824", "Sighard"])
+    tmp_arr[inc_idx] = ["817-824", "Sighard"]
+
+    # Fix overlap for Elven Male 271-273
+    inc_idx = tmp_arr.index(["271-280", "Ceoluulf"])
+    tmp_arr[inc_idx] = ["273-280", "Ceoluulf"]
+
+    # Fix overlap for Subterranean troll
+    inc_idx = tmp_arr.index(["185-192", "Eileithyia"])
+    tmp_arr[inc_idx] = ["185-191", "Eileithyia"]
+
+    return tmp_arr
+
+
+def get_names(arr: list[list[str]]):
+    tmp = []
+    last_idx = 0
+
+    for idx in range(1, len(arr)):
+        curr_name = arr[idx]
+        prev_name = arr[idx - 1]
+        last_idx = idx
+
+        # End of names
+        if len(curr_name) == 1:
+            if len(prev_name) == 2:
+                break
+            else:
+                continue
+        else:
+            tmp.append(curr_name.copy())
+
+    if not validate_list(tmp):
+        raise Exception  # noqa: TRY002
+
+    return tmp, last_idx
+
+
+def collect_get_names(arr):
+    tmp = []
+    in_arr = arr
+
+    while True:
+        names, idx = get_names(in_arr)
+        tmp.append(names)
+        in_arr = in_arr[idx:]
+
+        if len(in_arr) == 1:
+            break
+
+    return tmp
+
+
+# Check for missing entries
+def validate_list(arr: list[list[str]]) -> bool:
+    remaining_count = 1000
+    my_list = [x[0] for x in arr]
+
+    start = None
+    end = None
+
+    y_end = None
+    y_start = None
+
+    len2_count = 0
+    for idx in my_list:
+        splits = [int(x) for x in idx.split("-")]
+        # pp(remaining_count)
+        pp(splits)
+
+        if len(splits) == 1:
+            remaining_count -= 1
+            start = splits[0]
+            end = splits[0]
+            y_end = splits[0]
+            y_start = splits[0]
+        elif len(splits) == 2:
+            assert splits[1] > splits[0]
+
+            if len2_count % 2 == 0:
+                end = splits[1]
+                y_start = splits[0]
+
+                # Find Gaps
+                if (
+                    y_start is not None
+                    and y_end is not None
+                    and (
+                        abs(y_end - y_start) > 1 or y_end > y_start
+                    )  # Overlap and Gap check
+                ):
+                    raise ValueError(f"Gap found: {y_end} - {y_start}")
+            else:
+                start = splits[0]
+                y_end = splits[1]
+
+                if (
+                    start is not None
+                    and end is not None
+                    and (abs(end - start) > 1 or end > start)
+                ):
+                    raise ValueError(f"Gap found: {end} - {start}")
+
+            # Inclusive start and end.
+            if start == end or y_start == y_end:
+                raise ValueError(f"Inclusive start and end found: {start}")
+            else:
+                remaining_count -= (
+                    splits[1] - splits[0]
+                ) + 1  # Add 1 since end-inclusive
+
+            len2_count += 1
+        else:
+            raise ValueError("Not a range")
+
+    print(remaining_count)
+
+    # Name is either 1d1000 or 1d100
+    return bool(remaining_count == 0 or remaining_count == 900)
 
 
 def main() -> None:
@@ -52,95 +189,58 @@ def main() -> None:
     raw_names = []
 
     with pdfplumber.open(pdf_path) as pdf:
-        # page = pdf.pages[951]
-        # cropped_page = page.within_bbox((40, 40, page.width - 112, page.height - 40))
-        # cropped_page = page.within_bbox((0, 0, page.width - 50, page.height - 50))
-        # cropped_page.to_image().show()
         for n in range(947, 963):
             pages = parse_page(pdf.pages[n], n % 2 == 0)
             raw_names.extend(pages)
 
-    parsed_names = collect_pairs(raw_names)
+    paired_names = collect_pairs(raw_names)
+    paired_names = apply_fixes(paired_names)
 
-    human_male, rest = parse_list("Female", parsed_names)
-    human_female, rest = parse_list("Last", rest)
-    human_last, rest = parse_list("Bugbear", rest)
-
-    # Cut out mid-sentence
-    bugbear_male_pre, rest = parse_list("Suffi", rest)
-    bugbear_male_suf, rest = parse_list("Prefixes", rest)
-    bugbear_female_pre, rest = parse_list("Suffixes", rest)
-    bugbear_female_suf, rest = parse_list("Dwarven", rest)
-
-    dwarven_male, rest = parse_list("Female", rest)
-    dwarven_female, rest = parse_list("Elven", rest)
-
-    elven_male, rest = parse_list("Female", rest)
-    elven_female, rest = parse_list("Kobold", rest)
-
-    kobold_male, rest = parse_list("Female", rest)
-    kobold_female, rest = parse_list("Base", rest)
-
-    base_ogre_male_pre, rest = parse_list("Suf", rest)
-    base_ogre_male_suf, rest = parse_list("Cliff", rest)
-
-    cliff_ogre_male_pre, rest = parse_list("Suffixes", rest)
-    cliff_ogre_male_suf, rest = parse_list("Prefixes", rest)
-
-    grugach_male_pre, rest = parse_list("Suffixes", rest)
-    grugach_male_suf, rest = parse_list("Prefixes", rest)
-
-    kinderfresser_male_pre, rest = parse_list("Suffixes", rest)
-    kinderfresser_male_suf, rest = parse_list("Prefixes", rest)
-
-    borb_hill_pre, rest = parse_list("Suffixes", rest)
-    borb_hill_suf, rest = parse_list("Subterranean", rest)
-
-    sub_male, rest = parse_list("Female", rest)
-    sub_female, _rest = parse_list("", rest)
+    names = collect_get_names(paired_names)
+    pp(names)
 
     with open(output_path, "w") as fp:
         json.dump(
             {
                 "human": {
-                    "male": human_male,
-                    "female": human_female,
-                    "last": human_last,
+                    "male": names[0],
+                    "female": names[1],
+                    "last": names[2],
                 },
                 "bugbear": {
                     "male": {
-                        "pre": bugbear_male_pre,
-                        "suf": bugbear_male_suf,
+                        "pre": names[3],
+                        "suf": names[4],
                     },
-                    "female": {"pre": bugbear_female_pre, "suf": bugbear_female_suf},
+                    "female": {"pre": names[5], "suf": names[6]},
                 },
                 "dwarven": {
-                    "male": dwarven_male,
-                    "female": dwarven_female,
+                    "male": names[7],
+                    "female": names[8],
                 },
                 "elven": {
-                    "male": elven_male,
-                    "female": elven_female,
+                    "male": names[9],
+                    "female": names[10],
                 },
-                "kobold": {"male": kobold_male, "female": kobold_female},
+                "kobold": {"male": names[11], "female": names[12]},
                 "base_ogre": {
-                    "pre": base_ogre_male_pre,
-                    "suf": base_ogre_male_suf,
+                    "pre": names[13],
+                    "suf": names[14],
                 },
-                "cliff_ogre": {"pre": cliff_ogre_male_pre, "suf": cliff_ogre_male_suf},
+                "cliff_ogre": {"pre": names[15], "suf": names[16]},
                 "grugach_ogre": {
-                    "pre": grugach_male_pre,
-                    "suf": grugach_male_suf,
+                    "pre": names[17],
+                    "suf": names[18],
                 },
                 "kinderfresser": {
-                    "pre": kinderfresser_male_pre,
-                    "suf": kinderfresser_male_suf,
+                    "pre": names[19],
+                    "suf": names[20],
                 },
                 "borb_hill": {
-                    "pre": borb_hill_pre,
-                    "suf": borb_hill_suf,
+                    "pre": names[21],
+                    "suf": names[22],
                 },
-                "sub_troll": {"male": sub_male, "female": sub_female},
+                "sub_troll": {"male": names[23], "female": names[24]},
             },
             fp,
         )
